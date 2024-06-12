1cc648189ba39f6ffeff2c6e586b330d565492ad
Ashish Bhosle <asbhosle2003@gmail.com>
2024-06-12 16:53:00 +0530

    git push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import TestimonalCards from "@/components/TestimonalCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased
       bg-grid-white/[0.2]">
-        <h1 className="text-2xl text-center">Next with Ashish</h1>
+
         <HeroSection />
         <FeaturedCourses />
+        <WhyChooseUs />
+        <TestimonalCards />
+        <UpcomingWebinars />
+        <Instructors />
+        <Footer />
+        
     </main>
   )
 }
